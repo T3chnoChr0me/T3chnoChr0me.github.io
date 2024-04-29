085ac58e7139826dae85d1e6b7386cdf014b6d56
T3chnoChr0me <116015019+T3chnoChr0me@users.noreply.github.com>
2024-04-29 17:13:05 -0400

    Change write to append data instead of overwriting it.

--- save_command.php
+++ save_command.php
@@ -4,8 +4,14 @@ if(isset($_POST['text'])) {
     // Get the text data
     $text = $_POST['text'];
     
-    // Write the text data to a text file
-    file_put_contents('command_list.txt', $text);
+    // Open the file in append mode
+    $handle = fopen($file, 'a') or die('Cannot open file:  '.$file);
+   
+    // Append the text data to the file
+    fwrite($handle, $text . PHP_EOL); // Add a newline after each entry
+    
+    // Close the file handle
+    fclose($handle);
     
     // Send a success response
     http_response_code(200);
